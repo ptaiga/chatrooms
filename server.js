365320df83281864d35f649d751ae15191758c37
Pavel Taiga <ptaiga@gmail.com>
2016-09-20 12:25:00 +0300

    Added 3 function: send404, sendFile, serveStatic

--- server.js
+++ server.js
@@ -17,3 +17,44 @@ var mime = require('mime');
 // Объект cache реализует хранение содержимого 
 // кэшированных файлов
 var cache = {};
+
+function send404(response) {
+    response.writeHead(404, {'Content-Type': 'text/plain'});
+    response.write('Error 404: resourse not found.');
+    response.end();
+}
+
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(
+        200,
+        {"content-type": mime.lookup(path.basename(filePath)) }
+    );
+    response.end(fileContents);
+}
+
+function serveStatic(response, cache, absPath) {
+    // Проверка факта кэширования файла в памяти
+    if (cache[absPath]) {
+        // Обслуживание файла, находящегося в памяти
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        // Проверка факта существования файла
+        fs.exists(absPath, function(exists) {
+            if (exists) {
+                // Считывание файла с диска
+                fs.readFile(absPath, function(err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        // Обслуживание файла, считанного с диска
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                // Отсылка HTTP-ответа 404
+                send404(response);
+            }
+        });
+    }
+}
