d1705d1841bf5b3c2bfba1af704901e33ec43701
Pavel Taiga <ptaiga@gmail.com>
2016-09-21 11:18:42 +0300

    Added initialization logic on the client side

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -24,3 +24,59 @@ function processUserInput(chatApp, socket) {
     }
     $('#send-message').val('');
 }
+
+var socket = io.connect();
+
+$(document).ready(function() {
+    var chatApp = new Chat(socket);
+
+    // Вывод результатов попытки изменения имени
+    socket.on('nameResult', function(result) {
+        var message;
+        if (result.success) {
+            message = 'You are now known as ' + result.name + '.';
+        } else {
+            message = result.message;
+        }
+        $('#messages').append(divSystemContentElement(message));
+    });
+
+    // Вывод результатов изменения комнаты
+    socket.on('joinResult', function(result) {
+        $('#room').text(result.room);
+        $('#messages').append(divSystemContentElement('Room changed.'));
+    });
+
+    // Вывод полученных сообщений
+    socket.on('message', function (message) {
+        var newElement = $('<div></div>').text(message.text);
+        $('#messages').append(newElement);
+    });
+
+    // Вывод списка доступных комнат
+    socket.on('rooms', function(rooms) {
+        $('#room-list').empty();
+        for(var room in rooms) {
+            room = room.substring(1, room.length);
+            if (room != '') {
+                $('#room-list').append(divEscapedContentElement(room));
+            }
+        }
+        // Разрешено щелкнуть на имени комнаты, чтобы изменить ее
+        $('#room-list div').click(function() {
+            chatApp.processCommand('/join ' + $(this).text());
+            $('#send-message').focus();
+        });
+    });
+
+    // Запрос списка поочередно доступных комнат чата
+    setInterval(function() {
+        socket.emit('rooms');
+    }, 1000);
+    $('#send-message').focus();
+    // Отправка сообщений чата с помощью формы
+    $('#send-form').submit(function() {
+        processUserInput(chatApp, socket);
+        return false;
+    });
+});
