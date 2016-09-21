35a329daf0a85cebd1b6d7c47fc4df945699f94c
Pavel Taiga <ptaiga@gmail.com>
2016-09-21 11:06:47 +0300

    Added function processUserInput

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -5,3 +5,22 @@ function divEscapedContentElement(message) {
 function divSystemContentElement(message) {
     return $('<div></div>').html('<i>' + message + '</i>');
 }
+
+function processUserInput(chatApp, socket) {
+    var message = $('#send-message').val();
+    var systemMessage;
+    // Начинающиеся со слеша данные, вводимые пользователем,
+    // трактуются как команды
+    if (message.charAt(0) == '/') {
+        systemMessage = chatApp.processCommand(message);
+        if (systemMessage) {
+            $('#messages').append(divSystemContentElement(systemMessage));
+        }
+    } else {
+        // Трансляция вводимых пользователем данных другим пользователям
+        chatApp.sendMessage($('#room').text(), message);
+        $('#messages').append(divEscapedContentElement(message));
+        $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+    }
+    $('#send-message').val('');
+}
